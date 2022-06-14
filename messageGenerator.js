0d889eb582f88c15b7ffa034e827dd9c4afd95ff
Taylor Schrader <taylorschrader@Taylors-MacBook-Pro.local>
2022-06-14 00:39:52 -0500

    genenerate random 3 word sentence

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,9 +1,22 @@
 let startRandomList = ["One","Two","Three","Four","Five","Six","Seven","Eight","Nine",
                         "Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen",
                         "Seventeen","Eightteen","Nineteen","Twentry"];
-let middleRandomList = [" blind"," running"," drunk"," sleeping"," stunned"," tripping",
-                        " lifting"," falling"," scared"," living"," dead"," jumping"," slumped",
-                        " tired"," gaming"," programming"," adult",];
-let endRandomList = [" mice."," bags."," people."," oranges."," children."," elephants."," roaches.",
-                        " teachers."," men."," athletes."," officers."," chairmen."," plates.",];
+let middleRandomList = ["blind","running","drunk","sleeping","stunned","tripping",
+                        "lifting","falling","scared","living","dead","jumping","slumped",
+                        "tired","gaming","programming","adult",];
+let endRandomList = ["mice","bags","people","oranges","children","elephants","roaches",
+                        "teachers","men","athletes","officers","chairmen","plates",];
 
+const pickRandomNum = (max) =>{
+    return Math.floor(Math.random() * (max-1));
+}
+
+const generateRandomString = () =>{
+    let firstWord = startRandomList[pickRandomNum(startRandomList.length)];
+    let middleWorld = middleRandomList[pickRandomNum(middleRandomList.length)];
+    let endWord = endRandomList[pickRandomNum(endRandomList.length)];
+
+    return (`${firstWord} ${middleWorld} ${endWord}.`);
+}
+
+console.log(generateRandomString());
