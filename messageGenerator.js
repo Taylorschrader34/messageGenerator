709ae68a4f715d000abf4551cb02ea3e3d1e7992
Taylor Schrader <taylorschrader@Taylors-MacBook-Pro.local>
2022-06-14 00:21:04 -0500

    first commit

---
+++ messageGenerator.js
@@ -0,0 +1,9 @@
+let startRandomList = ["One","Two","Three","Four","Five","Six","Seven","Eight","Nine",
+                        "Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen",
+                        "Seventeen","Eightteen","Nineteen","Twentry"];
+let middleRandomList = [" blind"," running"," drunk"," sleeping"," stunned"," tripping",
+                        " lifting"," falling"," scared"," living"," dead"," jumping"," slumped",
+                        " tired"," gaming"," programming"," adult",];
+let endRandomList = [" mice."," bags."," people."," oranges."," children."," elephants."," roaches.",
+                        " teachers."," men."," athletes."," officers."," chairmen."," plates.",];
+
